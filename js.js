6168f1eb51fe7c92216de9e8c5abcdb746f87fdc
Evanc-1010 <107043407+Evanc-1010@users.noreply.github.com>
2022-10-25 15:00:17 +0800

    initial upload

---
+++ js.js
@@ -0,0 +1,7 @@
+let addbtn = document.createElement("button");
+addbtn.innerHTML = "Test";
+
+let btnarea = document.querySelector('.testbutton');
+btnarea.appendChild(addbtn);
+
+addbtn.onclick = ;
